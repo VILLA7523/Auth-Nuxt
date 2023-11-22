f70a8c3c178d059c6bdbae551483c7e826600efd
villa7523 <fvillarroel@unsa.edu.pe>
2023-11-22 10:53:49 -0500

    auth-nuxt

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtLayout repo="nuxt/examples"  example="auth/local"> 
+    <AppNav />
+    <div>
+      <NuxtPage />
+    </div>
+  </NuxtLayout> 
+</template>
